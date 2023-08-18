220f68236398d204663b607fb7eb67dfd5b0e3f7
prabirshub <prabirsinghdigital@gmail.com>
2023-08-18 09:55:49 +0530

    about data

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,27 @@
+import { useState } from 'react'
+
+// framer
+import { motion } from 'framer-motion'
+
+// variants
+import { fadeIn } from '../../variants'
+
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from 'react-icons/fa'
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from 'react-icons/si'
+
 //  data
 const aboutData = [
   {
@@ -6,18 +30,22 @@ const aboutData = [
       {
         title: 'Web Development',
         icons: [
-          // <FaHtml5 />,
-          // <FaCss3 />,
-          // <FaJs />,
-          // <FaReact />,
-          // <SiNextdotjs />,
-          // <SiFramer />,
-          // <FaWordpress />,
+          <FaHtml5 key='first' />,
+          <FaCss3 key='second' />,
+          <FaJs key='third' />,
+          <FaReact key='fourth' />,
+          <SiNextdotjs key='fifth' />,
+          <SiFramer key='sixth' />,
+          <FaWordpress key='seventh' />,
         ],
       },
       {
-        // title: 'UI/UX Design',
-        // icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        title: 'UI/UX Design',
+        icons: [
+          <FaFigma key='eight' />,
+          <SiAdobexd key='nine' />,
+          <SiAdobephotoshop key='tenth' />,
+        ],
       },
     ],
   },
@@ -70,8 +98,49 @@ const aboutData = [
   },
 ]
 
+// components
+import Avatar from '../../components/Avatar'
+import Circles from '../../components/Circles'
+
 const About = () => {
-  return <div>About</div>
+  const [index, setIndex] = useState(0)
+  console.log(index)
+  return (
+    <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+      <Circles />
+      {/* avatar */}
+      <motion.div
+        variants={fadeIn('right', 0.2)}
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className='hidden xl:flex absolute bottom-0 -left-[370px]'
+      >
+        <Avatar />
+      </motion.div>
+      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
+        <div>text</div>
+        <div>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default About
